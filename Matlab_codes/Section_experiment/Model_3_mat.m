07a84f7058465159765e2d322918b1993453e710
wangcaibing <1294464854@qq.com>
2021-03-11 11:17:25 +0800

    Create Model_3_mat.m

---
+++ Matlab_codes/Section_experiment/Model_3_mat.m
@@ -0,0 +1,21 @@
+clear;
+N = 2^19:2^18:2^24;
+for i=1:length(N)
+F=@(x,y)(exp(sqrt(x.*y)+sqrt(y-x.*y))+exp(sqrt(x.*y)-sqrt(y-x.*y))+exp(-sqrt(x.*y)+sqrt(y-x.*y))+exp(-sqrt(x.*y)-sqrt(y-x.*y))).*exp((-(N(i).^2).*2^(-35)-y)./(N(i)*2^(-17)))./(N(i)*2^(-15).*pi.*sqrt(x.*(1-x)));
+P(i)=1-(integral2(F,0,1,0,32));
+Q(i)=1-(integral2(F,0,1,0,128));
+O(i)=1-(integral2(F,0,1,0,288));
+R(i)=1-(integral2(F,0,1,0,512));
+end;
+X=log2(N);  
+plot(X,P,'g','Linewidth',1.7);
+hold on;
+plot(X,Q,'--r','Linewidth',1.3);
+plot(X,O,'-.b','Linewidth',1.3);
+plot(X,R,':m','Linewidth',1.3);
+ylim([0 1]);
+legend('T=32','T=128','T=288','T=512');
+set(gcf, 'PaperPosition', [-0.75 0.6 40 24]);
+set(gcf, 'PaperSize', [39 24]);
+saveas(gcf,'D:\1A所有文件\Model_3_mat.pdf')
+
